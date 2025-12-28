1a8c38a35fc9cd5b02086d2eb1b74e05bd965c82
Chris Keller <xylo04@gmail.com>
2025-12-28 11:32:31 -0700

    Fix firebase hosting

--- web/src/app/firebase.config.ts
+++ web/src/app/firebase.config.ts
@@ -5,5 +5,5 @@ export const firebaseConfig = {
   projectId: "arrl-co-yotc",
   storageBucket: "arrl-co-yotc.firebasestorage.app",
   messagingSenderId: "285425475616",
-  appId: "1:285425475616:web:c0dd8e29386ce5f0109c45"
+  appId: "1:285425475616:web:176b5013dce8f6f4109c45"
 };
